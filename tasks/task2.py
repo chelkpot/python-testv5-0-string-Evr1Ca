4bb5a1a63250ae57d0f2c0bc9964f718387922f8
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:19:28 +0500

    Update task2.py

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,8 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-   
-
+    phrase = input()
+    count = len(phrase)
+    print(count)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
