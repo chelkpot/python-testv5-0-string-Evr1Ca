0c9d3e39e2ed924e1340dc8b9823b39eff9e0a8d
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:20:37 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,9 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
-
-
+    first_string = input()
+    second_string = input()
+    result = second_string + first_string
+    print(result)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
