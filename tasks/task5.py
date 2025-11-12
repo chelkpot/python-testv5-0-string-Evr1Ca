e0886f00c0f13fe058128416fd3a1d5497270c5d
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:24:30 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    input_string = input()
+    characters = input_string.split()
+    print(f"Код символа {characters[0]} равен {ord(characters[0])}")
+    print(f"Код символа {characters[1]} равен {ord(characters[1])}")
+    print(f"Код символа {characters[2]} равен {ord(characters[2])}")
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
