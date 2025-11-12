9ab04895c1087285ca48d8a350a18b7a05ce43cb
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:24:43 +0500

    Update task4.py

--- tasks/task4.py
+++ tasks/task4.py
@@ -2,9 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-phrase = input()
-result = phrase * 3
-print(result)
+    phrase = input()
+    result = phrase * 3
+    print(result)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
